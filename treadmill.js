ab8c03620a74fa5e2d394c45812ac6ef0bfea811
Johnie Hjelm <johnie.hjelm@symbio.com>
2015-01-07 21:02:05 +0100

    Minor fix

--- treadmill.js
+++ treadmill.js
@@ -2,17 +2,12 @@
  * treadmill.js
  * Copyright © 2015 Johnie Hjelm
  */
-;(function () {
-  'use strict';
+var Treadmill = Treadmill || {};
 
-  var Treadmill = Treadmill || {};
-
-  Treadmill.run = function() {
-    window.onscroll = function() {
-      if ((window.innerHeight + window.scrollY) >= ((document.documentElement && document.documentElement.scrollHeight) || document.body.scrollHeight)) {
-        window.scrollTo(0,0);
-      }
-    };
+Treadmill.run = function() {
+  window.onscroll = function() {
+    if ((window.innerHeight + window.scrollY) >= ((document.documentElement && document.documentElement.scrollHeight) || document.body.scrollHeight)) {
+      window.scrollTo(0,0);
+    }
   };
-
-})();
+};
