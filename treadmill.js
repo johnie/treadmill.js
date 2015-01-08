a19b61fc92e7ac82b114a593699ea7625767e6c7
Fredrik Forsmo <frozzare@users.noreply.github.com>
2015-01-08 16:19:59 +0100

    Add support for AMD, CommonJS and global export
    :smile:

--- treadmill.js
+++ treadmill.js
@@ -2,12 +2,31 @@
  * treadmill.js
  * Copyright © 2015 Johnie Hjelm
  */
-var Treadmill = Treadmill || {};
 
-Treadmill.run = function() {
-  window.onscroll = function() {
-    if ((window.innerHeight + window.scrollY) >= ((document.documentElement && document.documentElement.scrollHeight) || document.body.scrollHeight)) {
-      window.scrollTo(0,0);
+'use strict';
+
+(function (root, factory) {
+  if (typeof define === 'function' && define.amd) {
+    define([], factory);
+  } else if (typeof exports === 'object') {
+		exports.Treadmill = factory();
+	} else {
+    root.Treadmill = factory();
+  }
+}(this, function () {
+  return {
+		
+		/**
+		 * Attach function to window.onscroll and run Treadmill.
+		 */
+		
+    run: function () {
+      window.onscroll = function() {
+        if ((window.innerHeight + window.scrollY) >= ((document.documentElement && document.documentElement.scrollHeight) || document.body.scrollHeight)) {
+          window.scrollTo(0, 0);
+        }
+      };
     }
+
   };
-};
+}));
